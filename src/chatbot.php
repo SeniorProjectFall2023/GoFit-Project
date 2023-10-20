6767bffbd6c320f48e9363442f49c24e9ef4cf98
Fayzan Bhatti <fayzan23@gmail.com>
2023-10-19 20:00:43 -0500

    Made the chatbot history to be working, where the user could log out and log back in or switch between pages and their history is sitll there

--- src/chatbot.php
+++ src/chatbot.php
@@ -1,60 +1,56 @@
 <?php
-
 // Initialize a PHP session (if not already started)
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-$db_host = "localhost";
-$db_user = "root";
-$db_password = "cosc4360-McCurry";
-$db_name = "GoFit";
+// Check if the user is logged in and has a valid user ID
+if (isset($_SESSION['userID']) && is_numeric($_SESSION['userID'])) {
+    $userId = $_SESSION['userID'];
+    
+    // Your database connection code here
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_password = "cosc4360-McCurry";
+    $db_name = "GoFit";
 
-// Create a database connection
-$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+    // Create a database connection
+    $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-// Retrieve chat data from the POST request
-$data = file_get_contents("php://input");
-$messageData = json_decode($data);
+    // Retrieve chat data from the POST request
+    $data = file_get_contents("php://input");
+    $messageData = json_decode($data);
 
-if ($messageData) {
-    $userMessage = $messageData->userMessage;
-    $chatbotResponse = $messageData->chatbotResponse;
+    if ($messageData) {
+        $userMessage = $messageData->userMessage;
+        $chatbotResponse = $messageData->chatbotResponse;
 
-    // Prepare an SQL statement to insert the messages into the chat_messages table
-    $sql = "INSERT INTO chat_messages (user_message, chatbot_response) VALUES (?, ?)";
-    $stmt = $conn->prepare($sql);
+        // Prepare an SQL statement to insert the messages into the chat_messages table
+        $sql = "INSERT INTO chat_messages (user_message, chatbot_response, user_id) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($sql);
 
-    if ($stmt) {
-        $stmt->bind_param("ss", $userMessage, $chatbotResponse);
+        if ($stmt) {
+            $stmt->bind_param("ssi", $userMessage, $chatbotResponse, $userId);
 
-        // Execute the SQL statement
-        if ($stmt->execute()) {
-            echo "Messages saved successfully!";
+            // Execute the SQL statement
+            if ($stmt->execute()) {
+                echo "Messages saved successfully!";
+            } else {
+                echo "Error executing the SQL statement: " . $stmt->error;
+            }
+
+            $stmt->close();
         } else {
-            echo "Error executing the SQL statement: " . $stmt->error;
+            echo "Error preparing the SQL statement: " . $conn->error;
         }
-
-        $stmt->close();
-    } else {
-        echo "Error preparing the SQL statement: " . $conn->error;
     }
 
-    // Store the chat history in a session variable
-    if (!isset($_SESSION['chat_history'])) {
-        $_SESSION['chat_history'] = [];
-    }
-
-    // Append the user message and chatbot response to the chat history
-    $_SESSION['chat_history'][] = [
-        'user_message' => $userMessage,
-        'chatbot_response' => $chatbotResponse,
-    ];
+    $conn->close();
+} else {
+    echo "User is not logged in or has an invalid user ID.";
 }
-
-$conn->close();
 ?>
--- src/get_chat_history.php
+++ src/get_chat_history.php
@@ -1,10 +1,59 @@
 <?php
 session_start();
+
 header('Content-Type: application/json');
 
-if (isset($_SESSION['chat_history'])) {
-    echo json_encode($_SESSION['chat_history']);
+// Check if the user is logged in and has a valid user ID
+if (isset($_SESSION['userID']) && is_numeric($_SESSION['userID'])) {
+    $userId = $_SESSION['userID'];
+    
+    // Your database connection code here
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_password = "cosc4360-McCurry";
+    $db_name = "GoFit";
+
+    // Create a database connection
+    $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prepare an SQL statement to retrieve chat history for the user
+    $sql = "SELECT user_message, chatbot_response FROM chat_messages WHERE user_id = ?";
+    $stmt = $conn->prepare($sql);
+
+    if ($stmt) {
+        $stmt->bind_param("i", $userId);
+
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            $chatHistory = [];
+
+            while ($row = $result->fetch_assoc()) {
+                $chatHistory[] = [
+                    'user_message' => $row['user_message'],
+                    'chatbot_response' => $row['chatbot_response']
+                ];
+            }
+
+            echo json_encode($chatHistory);
+        } else {
+            // Debugging statement
+            echo json_encode(['error' => 'Execution of SQL statement failed.']);
+        }
+
+        $stmt->close();
+    } else {
+        // Debugging statement
+        echo json_encode(['error' => 'SQL statement preparation failed.']);
+    }
+
+    $conn->close();
 } else {
-    echo json_encode([]);
+    // Debugging statement
+    echo json_encode(['error' => 'User not logged in or invalid user ID.']);
 }
 ?>
+
